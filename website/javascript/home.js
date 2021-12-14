619cd524fff36394f1af53ae2b390460d7471b77
Daniel M. Jönsson <Daniel.macheka@live.se>
2021-12-14 13:35:20 +0100

    Patientsidor inkluderar nu enkel alarmknapp. + lite nya kommenterar i koden.

---
+++ website/javascript/home.js
@@ -0,0 +1,2 @@
+//Används för närvarande inte, javascripten ligger under <script> tags i html dokumenten.
+//Kodens ska flyttas hit egentligen men för tiden så ligger de kvar i sina html filer.
--- website/javascript/loginpage.js
+++ website/javascript/loginpage.js
@@ -1,4 +1,8 @@
-function loginButton(){
+
+//Används för närvarande inte, javascripten ligger under <script> tags i html dokumenten.
+//Kodens ska flyttas hit egentligen men för tiden så ligger de kvar i sina html filer.
+
+function loginButton() {
 	var adminName="oopa21"
 	var adminPass="1234"
 	var uName =document.getElementById("username").value;
